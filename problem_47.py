25ae2b9b56fb5bcbcb1dd47c0532e7e128046268
Bryce Hathaway <bryce@earthmine.com>
2018-10-03 10:18:18 -0700

    Refactored per prime library.

--- problem_47.py
+++ problem_47.py
@@ -1,14 +1,4 @@
-def prime_factors(n):
-    factors = []
-    d = 2
-    while n != 1:
-        if n % d != 0:
-            d = d + 1
-        else:
-            factors.append(d)
-            n = n / d
-
-    return factors
+from prime import prime_factors
 
 distinct_primes = [1, 1, 1, 1]
 
