91f8dac8d9ebbcbef6012392aa2adc2c9936ebf0
Bryce Hathaway <bryce@earthmine.com>
2018-10-03 10:01:16 -0700

    Turns out prime_factors weren't actually needed to solve the problem.

--- problem_29.py
+++ problem_29.py
@@ -1,15 +1,3 @@
-def prime_factors(n):
-    factors = []
-    d = 2
-    while n != 1:
-        if n % d != 0:
-            d = d + 1
-        else:
-            factors.append(d)
-            n = n / d
-
-    return factors
-
 x = set()
 
 for a in range(2, 101):
